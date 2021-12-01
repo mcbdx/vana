6c30926d261a2eb7a5fa760f9359a74abb9614f7
mauricastaneda@outlook.com <mauricastaneda@outlook.com>
2021-12-01 21:14:22 +0000

    Test Notebook

---
+++ NotebookTest.py
@@ -0,0 +1,44 @@
+# Databricks notebook source
+df = spark.read.parquet("/databricks-datasets/amazon/test4K")
+display(df)
+
+# COMMAND ----------
+
+import os 
+
+os.getcwd()
+
+# COMMAND ----------
+
+print(os.getcwd() + "/demo.csv")
+
+# COMMAND ----------
+
+import csv
+with open(os.getcwd() + "/demo.csv", 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
+
+# COMMAND ----------
+
+import databricks.koalas as kd
+
+df_koalas = kd.read_csv(f"file:{os.getcwd()}/demo.csv")
+display(df_koalas)
+
+# COMMAND ----------
+
+csv_df = spark.read.csv(f"file:{os.getcwd()}/demo.csv", header=True) # "file:" prefix and absolute file path are required for PySpark
+display(csv_df)
+
+# COMMAND ----------
+
+from reader import csv_reader
+
+df = csv_reader("demo.csv")
+print(df.head())
+
+# COMMAND ----------
+
+
